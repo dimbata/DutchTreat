10d89676227a6e60d2cdc4413d3518b965abd789
Dimitar Filipov <dfilipov94@gmail.com>
2018-04-08 14:55:39 +0300

    implemented the shop angular component

--- DutchTreat/Controllers/AppController.cs
+++ DutchTreat/Controllers/AppController.cs
@@ -61,8 +61,7 @@ namespace DutchTreat.Controllers
         [Authorize]
         public IActionResult Shop()
         {
-            var results = _repository.GetAllProducts();
-            return View(results);
+            return View();
         }
     }
 }
